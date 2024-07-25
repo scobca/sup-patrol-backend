ebd089292f995fe6b4a2c0db9bd812a99ad30bf4
Vladimir <115186975+scobca@users.noreply.github.com>
2024-07-26 01:44:57 +0300

    connection to database

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controller/app.controller';
 import { AppProvider } from './providers/app.provider';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { dbConf } from '../../conf/db.conf';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      ...dbConf,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppProvider],
 })
